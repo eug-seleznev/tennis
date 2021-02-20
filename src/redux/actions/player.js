c1e4e4012538f47d2f2e818adb753864191a4da2
alexdcrv <bloodlydeadknight@gmail.com>
2021-02-20 23:17:50 +0300

    stat

--- src/redux/actions/player.js
+++ src/redux/actions/player.js
@@ -1,8 +1,51 @@
 import {innerBackend} from '../../components/utils/axios'
 
-import { CLIENT_ERROR, EDIT_PROFILE } from "../types";
+import { ALL_PLAYERS, CLIENT_ERROR, EDIT_PROFILE,MY_INFO } from "../types";
 
 
+
+export const allPlayers = (find, city) => async (dispatch) => {
+  try {
+    const res = await innerBackend.get(`/players/find?field=${find='all'?find:find+'&value='+city}`);
+
+    dispatch({
+      type: ALL_PLAYERS,
+      payload: res.data,
+    });
+
+    console.log(res.data, 'respnse')
+
+  } catch (err) {
+      console.log(err.response.data)
+    const errors = err.response.data;
+    dispatch({
+      type: CLIENT_ERROR,
+      payload: errors,
+    });
+  }
+};
+
+
+export const myInfo = () => async (dispatch) => {
+  try {
+    const res = await innerBackend.get('/players/me');
+
+    dispatch({
+      type: MY_INFO,
+      payload: res.data,
+    });
+
+    console.log(res.data, 'respnse')
+
+  } catch (err) {
+      console.log(err.response.data)
+    const errors = err.response.data;
+    dispatch({
+      type: CLIENT_ERROR,
+      payload: errors,
+    });
+  }
+};
 
 
 
