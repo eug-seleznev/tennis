34e50917f34b9a19e27c1f9e1198ea62144a464b
alexdcrv <bloodlydeadknight@gmail.com>
2021-02-21 16:56:21 +0300

    game stat

--- src/screens/Player.js
+++ src/screens/Player.js
@@ -8,12 +8,70 @@ import { ScrollView } from 'react-native';
 const Player = () => {
   const playersList = useSelector(state=> state.player.allPlayers)
   const myInf = useSelector(state=> state.player.myInfo)
+  let myGames = [
+    {
+      result:'win',
+      enemy:'defoltniy tennisman',
+      date:'20.10.20',
+      number:'1488',
+      rank:122,
+    },
+    {
+      result:'win',
+      enemy:'retard',
+      date:'23.10.20',
+      number:'1438',
+      rank:557,
+    },
+    {
+      result:'win',
+      enemy:'chmo',
+      date:'27.10.20',
+      number:'1418',
+      rank:278,
+    },
+    {
+      result:'win',
+      enemy:'pidr',
+      date:'30.10.20',
+      number:'1411',
+      rank:222,
+    },
+    {
+      result:'lose',
+      enemy:'grandmaster',
+      date:'30.10.20',
+      number:'1337',
+      rank:15,
+    },
+    {
+      result:'lose',
+      enemy:'atp player',
+      date:'30.10.20',
+      number:'1334',
+      rank:3,
+    },
+    {
+      result:'lose',
+      enemy:'regular',
+      date:'30.10.20',
+      number:'1333',
+      rank:30,
+    },
+    {
+      result:'win',
+      enemy:'defoltniy tennisman',
+      date:'30.10.20',
+      number:'1311',
+      rank:156,
+    }
+  ]
   const [find, setFind] = useState('all')
   const [city, setCity] = useState(false)
   let dispatch = useDispatch()
   useEffect(()=>{
     dispatch(myInfo())
-    
+   
   },[])
   useEffect(()=>{
     console.log(myInf,'myInfo')
@@ -33,18 +91,17 @@ const Player = () => {
     
       <DataTable.Header style={styles.header}>
         
-          <DataTable.Title>Игрок</DataTable.Title>
+          <DataTable.Title >     Игрок</DataTable.Title>
           <DataTable.Title numeric>Рейтинг</DataTable.Title>
       </DataTable.Header>
       <ScrollView style={styles.table}>
       <DataTable >
       
-      {!playersList?<Text>none</Text>:
+      {!playersList?<Text>loading</Text>:
         playersList.map ((el,i)=>{
           return(
             <DataTable.Row key={i} style={styles.player}>
-              <DataTable.Cell style={{width:20}}>{i+1}</DataTable.Cell>
-              <DataTable.Cell>{el.fullname}</DataTable.Cell>
+              <DataTable.Cell>{i+1}  {el.fullname}</DataTable.Cell>
               <DataTable.Cell numeric>{el.rating}</DataTable.Cell>
             </DataTable.Row>
           )
@@ -52,6 +109,38 @@ const Player = () => {
       }
     </DataTable>
     </ScrollView>
+    <View style={styles.table}>
+      <View style={styles.top__info}>
+        <Text>История игр</Text>
+        <Text>
+          <Text style={{color:'green'}}>{myInf.win}</Text>/
+          <Text style={{color:'red'}}>{myInf.defeat}</Text>
+          <Text style={{color:`${myInf.win/myInf.defeat*100>50?'green':'red'}`}}> {myInf.win/myInf.defeat*100+'%'}</Text>
+        </Text>
+      </View>
+      <DataTable.Header style={styles.header}>
+          <DataTable.Title >Номер        Дата</DataTable.Title>
+          
+          <DataTable.Title numeric>Соперник</DataTable.Title>
+          <DataTable.Title numeric>Результат</DataTable.Title>
+      </DataTable.Header>
+      <ScrollView style={styles.table}>
+      <DataTable >
+      
+      {!myGames?<Text>loading</Text>:
+        myGames.map ((el,i)=>{
+          return(
+            <DataTable.Row key={i} style={styles.player}>
+              <DataTable.Cell >#{el.number}   {el.date}</DataTable.Cell>
+              <DataTable.Cell >#{el.rank} {el.enemy}</DataTable.Cell>
+              <Text style={{color:`${el.result==='win'?'green':'red'}`}}  numeric>{el.result}</Text>
+            </DataTable.Row>
+          )
+        })
+      }
+    </DataTable>
+    </ScrollView>
+    </View>
     </View>
   );
 }
@@ -61,17 +150,22 @@ const styles = StyleSheet.create({
     container: {
       backgroundColor: 'white',
       justifyContent: 'center',
-     
-      
+    },
+    top__info: {
+      width:'93%',
+      marginLeft:15,
+      display:'flex',
+      justifyContent:'space-between',
+      flexDirection:'row',
     },
     header: {
       display:'flex',
-      justifyContent: 'space-between',
+      justifyContent:'space-between'
 
     },
     table: {
       height:'40%',
-     
+      marginBottom:20
     },
     
     buttons_container: {
